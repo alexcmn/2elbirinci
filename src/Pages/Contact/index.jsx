5e42cbe4ee1eda5a5e0b8557d20a628b447e25d9
Aleksandar <aleksandar.milakovic@igamingplatform.com>
2021-11-22 17:44:14 +0100

    addiotional translation added

--- src/Pages/Contact/index.jsx
+++ src/Pages/Contact/index.jsx
@@ -7,16 +7,20 @@ import { Col, Container, Row } from 'react-bootstrap'
 import { StyledHeadline } from 'Components/shared/styledComponents/Elements';
 import { FiMapPin, FiPhoneCall, FiMail } from 'react-icons/fi';
 import { ImWhatsapp } from 'react-icons/im';
+import { translate } from 'react-switch-lang';
 
 const LocationMap = withScriptjs(withGoogleMap(PropMap));
 
-export default function Contact() {
+function Contact(props) {
+
+    const { t } = props;
+
     return (
         <StyledContact>
             <div className="banner">
                 <img className="car-img" src={Car} alt="" />
                 <div className="main-head">
-                    <h1>Contact US</h1>
+                    <h1>{t('sidebar.li5')}</h1>
                 </div>
             </div>
             <div className="contact">
@@ -31,13 +35,13 @@ export default function Contact() {
                             />
                         </Col>
                         <Col md={6} className="info">
-                            <p>İnciroğlu Otomotiv A.Ş.</p>
-                            <StyledHeadline data-text="İnciroglu 2nd Hand First">İnciroglu 2nd Hand First</StyledHeadline>
+                            <p>{t('contact.comp')}</p>
+                            <StyledHeadline data-text={t('contact.h1')}>{t('contact.h1')}</StyledHeadline>
                             <ul>
-                                <li><FiMapPin/> Address: ANKARA YOLU 12. KM. AMBAR MEVKİ / KAYSERİ</li>
-                                <li><FiPhoneCall/> Phone: 0 352 311 40 00</li>
-                                <li><ImWhatsapp/> WhatsApp: 0 554 537 26 00</li>
-                                <li><FiMail/> Mail: info@incirogluikinciel.com</li>
+                                <li><FiMapPin/> {t('contact.address')}: ANKARA YOLU 12. KM. AMBAR MEVKİ / KAYSERİ</li>
+                                <li><FiPhoneCall/> {t('contact.phone')}: 0 352 311 40 00</li>
+                                <li><ImWhatsapp/> {t('contact.wp')}: 0 554 537 26 00</li>
+                                <li><FiMail/> {t('contact.mail')}: info@incirogluikinciel.com</li>
                             </ul>
                         </Col>
                     </Row>
@@ -46,3 +50,5 @@ export default function Contact() {
         </StyledContact>
     )
 }
+
+export default translate(Contact);
